102d1bfc349364a5cfd447609cd39f8859d9a38e
Łukasz Maksymczuk <lukasz@lukasz-maksymczuk.intra.codilime.com>
2019-02-21 12:56:26 +0100

    try

--- cloudify_cli/commands/admin.py
+++ cloudify_cli/commands/admin.py
@@ -1,3 +1,4 @@
+import json
 
 from ..cli import cfy
 from ..table import print_data
@@ -28,7 +29,8 @@ def config_get(client, logger):
 @config.command(name='set')
 @cfy.pass_client()
 @cfy.argument('option-name')
+@cfy.argument('option-value')
 @cfy.options.inputs
 @cfy.pass_logger
-def config_update(client, logger, option_name, inputs):
-    client.manager.put_config(option_name, inputs)
+def config_update(client, logger, option_name, option_value):
+    client.manager.put_config(option_name, json.loads(option_value))
