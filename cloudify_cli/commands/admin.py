e89ba46fc568ffbb9f5f19baf265e40e5698dca3
Łukasz Maksymczuk <lukasz@lukasz-maksymczuk.intra.codilime.com>
2019-02-21 12:54:16 +0100

    ye

--- cloudify_cli/commands/admin.py
+++ cloudify_cli/commands/admin.py
@@ -1,5 +1,8 @@
 
 from ..cli import cfy
+from ..table import print_data
+
+CONFIG_COLUMNS = ['name', 'value', 'updated_at']
 
 
 @cfy.group(name='admin')
@@ -19,4 +22,13 @@ def config():
 @cfy.pass_logger
 def config_get(client, logger):
     config = client.manager.get_config()
-    print config
+    print_data(CONFIG_COLUMNS, config, 'Manager config:')
+
+
+@config.command(name='get')
+@cfy.pass_client()
+@cfy.argument('option-name')
+@cfy.options.inputs
+@cfy.pass_logger
+def config_update(client, logger, option_name, inputs):
+    client.manager.put_config(option_name, inputs)
