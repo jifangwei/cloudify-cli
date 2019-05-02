80b3d7de8aac16e73f0ba04191f1c7a3bfca9f8b
asdf <tehasdf@gmail.com>
2019-05-02 10:58:19 +0200

    update

--- cloudify_cli/commands/config.py
+++ cloudify_cli/commands/config.py
@@ -16,3 +16,13 @@ def config():
 def list_config(client):
     configs = client.manager.get_config()
     print_data(CONFIG_COLUMNS, configs, 'Config:')
+
+
+@config.command(name='update')
+@cfy.pass_client()
+@cfy.pass_logger
+@cfy.argument('name')
+@cfy.argument('value')
+def update_config(client, name, value, logger):
+    client.manager.put_config(name, value)
+    logger.info('Set %s to %s', name, value)
